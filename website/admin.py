343e5ffe64be1e5bb3eba50a25c02cbf3b12c0f6
tWilltp <williamjenkinsfeb@gmail.com>
2023-05-30 13:15:06 +0000

    add Reviews model to admin panel, installed summernote and linked it up, add test review to check it worked, add list filter

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Reviews
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Reviews)
+class ReviewsAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
